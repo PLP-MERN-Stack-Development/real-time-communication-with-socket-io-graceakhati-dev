1309f734676c83ddba7eee3093203d0605b87589
Grace Akhati <graceakhati2@gmail.com>
2025-11-06 12:10:09 +0300

    server: init express + socket.io skeleton

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-center p-8">
+        Real-time Chat App
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
