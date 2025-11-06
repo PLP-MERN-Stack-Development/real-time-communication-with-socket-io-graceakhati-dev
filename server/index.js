a26f8523813a7ff680a10aafe33cd48441fddba1
Grace Akhati <graceakhati2@gmail.com>
2025-11-06 12:16:50 +0300

    client: init react + socket client provider

--- server/index.js
+++ server/index.js
@@ -1,3 +1,18 @@
-// Entry point for the server
-require('./server.js');
+const express = require('express');
+
+const app = express();
+
+// GET "/" route
+app.get('/', (req, res) => {
+  res.send('Server running');
+});
+
+// Start server
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log('Server listening on port 5000');
+});
+
+// Export app for testing
+module.exports = app;
 
